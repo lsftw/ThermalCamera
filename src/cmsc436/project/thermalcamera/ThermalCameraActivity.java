8e1ba4cd7a08c835ff7cbeffa16d41e4294af46f
Shiliang Zhang <lsftw13@gmail.com>
2014-11-20 23:37:38 -0500

    Added activities for gallery and camera.

---
+++ src/cmsc436/project/thermalcamera/ThermalCameraActivity.java
@@ -0,0 +1,14 @@
+package cmsc436.project.thermalcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+// TODO take picture, overlay sensor data
+public class ThermalCameraActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main); // TODO different layout
+	}
+}
