118d9a9bdb324a9b13f51693c20c94d10bdef984
yeealex <yeealex93@gmail.com>
2014-11-23 21:09:04 -0500

    removed hello world from strings.xml. main_activity.xml should now have a TODO string instead of Hello World.

---
+++ src/cmsc436/project/thermalcamera/InputTemperatureActivity.java
@@ -0,0 +1,14 @@
+package cmsc436.project.thermalcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputTemperatureActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.input_temperature);
+		
+	}
+}
