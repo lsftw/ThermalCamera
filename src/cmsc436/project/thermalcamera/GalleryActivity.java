a41248d16893cc42f709979147b64838a2d84367
yeealex <yeealex93@gmail.com>
2014-11-23 20:39:48 -0500

    testing test 1 2 3

--- src/cmsc436/project/thermalcamera/GalleryActivity.java
+++ src/cmsc436/project/thermalcamera/GalleryActivity.java
@@ -10,5 +10,6 @@ public class GalleryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main); // TODO different layout
+		//TODO
 	}
 }
